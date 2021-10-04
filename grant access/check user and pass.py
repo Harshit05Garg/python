8202d3b576f174edf350a7e0abf9a2d7f2d9544a
Yuvraj Garg <harshit05garg@gmail.com>
2021-10-04 15:42:59 +0530

    Update check user and pass.py
    Adding functionality of employee creation

--- grant access/check user and pass.py
+++ grant access/check user and pass.py
@@ -1,3 +1,13 @@
+def employee_creation():
+    employee = []
+    password = []
+    while True:
+        employee.append(input("Enter the new employee's name: "))
+        password.append(input("Enter the new password name: "))
+        print(employee[-1])
+        print(password[-1])
+
+
 def check_user_and_pass():
     admin_username = "kaku"
     admin_password = "harsh"
@@ -5,6 +15,9 @@ def check_user_and_pass():
     password = input("Enter the password: ")
     if username == admin_username and password == admin_password:
         print("Correct")
+        employee_creation()
     else:
         print("Invalid username or password")
+
+
 check_user_and_pass()
